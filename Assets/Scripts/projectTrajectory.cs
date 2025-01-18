b82dc0e4dc0aa8349af9794e3f4c70a3c660b1df
xr-jam <xr-jam@A65-08.etd-l.esiea-ouest.fr>
2025-01-18 15:42:00 +0100

    added trajectory planning

---
+++ Assets/Scripts/projectTrajectory.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(LineRenderer))]
+public class projectTrajectory : MonoBehaviour
+{
+    public float velocity;
+    public float baseVelocity;
+    public float maxVelocity;
+    public float gravity;
+    public Transform launchPosition;
+    public LineRenderer lineRenderer;
+    public int maxPoints;
+    public bool isProjecting;
+    public float currentHold;
+    public float maxHold;
+    public InputActionReference holdButton;
+    public InputActionReference holdReleaseButton;
+    public GameObject hitPointMarker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        isProjecting = false;
+        lineRenderer.enabled = false;
+        holdButton.action.performed += ctx => HoldTrigger();
+        holdReleaseButton.action.performed += ctx => HoldReleaseTrigger();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (isProjecting)
+        {
+            project();
+            // increase velocity based on how long the button is held
+            currentHold += Time.fixedDeltaTime;
+            if (currentHold > maxHold)
+            {
+                currentHold = maxHold;
+            }
+            if (currentHold > 0.5f)
+            {
+                velocity = (currentHold + 0.5f) * maxVelocity / maxHold;
+            }
+            
+            Debug.Log("velocity: " + velocity);
+        }
+    }
+
+    public void project()
+    {
+        Debug.Log("projecting trajectory");
+        // Set the line renderer to be visible
+        lineRenderer.enabled = true;
+        // The idea is to project the trajectory of the object till it hits the ground
+        // the trajectory is forward with gravity acting on it
+        lineRenderer.positionCount = maxPoints;
+        Vector3[] positions = new Vector3[maxPoints];
+        Vector3 velocityVector = launchPosition.forward * velocity;
+        Vector3 currentVelocity = velocityVector;
+        Vector3 currentPosition = launchPosition.position;
+        positions[0] = currentPosition;
+        for (int i = 1; i < maxPoints; i++)
+        {
+            // Calculate the new position
+            currentPosition += currentVelocity * Time.fixedDeltaTime;
+            // Calculate the new velocity
+            currentVelocity -= Vector3.up * gravity * Time.fixedDeltaTime;
+            positions[i] = currentPosition;
+            // Check if the object has hit something
+            //TODO : change with a collision check
+            Vector3 previouspoint = positions[i - 1];
+            Vector3 currentpoint = positions[i];
+            Vector3 segmentDirection = (currentpoint - previouspoint).normalized;
+
+            Ray ray = new Ray(previouspoint, segmentDirection);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Vector3.Distance(previouspoint, currentpoint)))
+            {
+                // If the object has hit something, stop projecting
+                lineRenderer.positionCount = i + 1;
+                positions[i] = hit.point;
+                // display the hit point
+                hitPointMarker.SetActive(true);
+                hitPointMarker.transform.position = hit.point;
+                break;
+            } else
+            {
+                hitPointMarker.SetActive(false);
+            }
+        }
+        lineRenderer.SetPositions(positions);
+    }
+
+    public void HoldReleaseTrigger()
+    {
+        isProjecting = false;
+        hitPointMarker.SetActive(false);
+        lineRenderer.enabled = false;
+        currentHold = 0f;
+        velocity = baseVelocity;
+    }
+
+    public void HoldTrigger()
+    {
+        isProjecting = true;
+    }
+}

50b648a6de9934c0f9e2c4b787d02c58cb50b5ec
xr-jam <xr-jam@A65-08.etd-l.esiea-ouest.fr>
2025-01-19 11:43:36 +0100

    score

--- Assets/Ressources/Scripts/ScoreManager.cs
+++ Assets/Ressources/Scripts/ScoreManager.cs
@@ -61,6 +61,7 @@ public class ScoreManager : MonoBehaviour
         {
             value = (score + redEnd) / (greenEnd + redEnd);
         }
+        slider.value = value;
         slider.GetComponent<FaceDisplayer>().onValueChanged(value);
 
         if (score <= -30)

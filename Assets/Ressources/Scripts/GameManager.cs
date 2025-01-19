54c2606d29acbea44fc9492d4298e17c98a76a95
xr-jam <xr-jam@A65-08.etd-l.esiea-ouest.fr>
2025-01-19 03:02:06 +0100

    added gameManager

---
+++ Assets/Ressources/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int level;
+
+    private void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadNextLevel()
+    {
+        level++;
+        LoadScene("GameScene");
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void resetLevel()
+    {
+        LoadScene("GameScene");
+    }
+
+    public void LoadMenu()
+    {
+        LoadScene("MenuScene");
+    }
+
+    public void LoadGame(SelectEnterEventArgs args)
+    {
+        level = 0;
+        Debug.Log(args.interactorObject.transform.parent.gameObject.name);
+    }
+}
